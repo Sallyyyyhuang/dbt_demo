3ffb32f3e6f1fa6763ae5a269eb7b380a2914621
Sally Huang <yuhsuan.huang@microchip.com>
2025-05-16 18:00:05 +0000

    modified staging model to add a transformed layer

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,123 +1,89 @@
---Import CTEs
-
-with 
-
-customers as (
-
-    select * from {{ source('jaffle_shop', 'customers') }}
-
-),
+with
 
 orders as (
 
-    select * from {{ source('jaffle_shop', 'orders') }}
+    select * from {{ ref('int_orders') }}
 
 ),
 
-payments as (
+customers as (
 
-    select * from {{ source('stripe', 'payment') }}
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
---Logical CTEs
-
---Final CTEs
-
-select 
-
-    orders.id as order_id,
-    orders.user_id as customer_id,
-    last_name as surname,
-    first_name as givenname,
-    first_order_date,
-    order_count,
-    total_lifetime_value,
-    round(amount/100.0,2) as order_value_dollars,
-    orders.status as order_status,
-    payments.status as payment_status
-
-from raw.jaffle_shop.orders as orders
-
-join (
-      select 
-        first_name || ' ' || last_name as name, 
-        * 
-      from customers
-) customers
-on orders.user_id = customers.id
-
-join (
+customer_order_history as (
 
     select 
-        b.id as customer_id,
-        b.name as full_name,
-        b.last_name as surname,
-        b.first_name as givenname,
-        min(order_date) as first_order_date,
 
-        min(case 
-            when a.status not in ('returned','return_pending') 
-            then order_date 
-        end) as first_non_returned_order_date,
+        customers.customer_id,
+        customers.full_name,
+        customers.surname,
+        customers.givenname,
 
-        max(case 
-            when a.status not in ('returned','return_pending') 
-            then order_date 
-        end) as most_recent_non_returned_order_date,
+        min(orders.order_date) as first_order_date,
 
-        coalesce(max(user_order_seq),0) as order_count,
+        min(orders.valid_order_date) as first_non_returned_order_date,
 
-        coalesce(count(case
-            when a.status != 'returned' 
-            then 1 end)
-            ,0
-        ) as non_returned_order_count,
+        max(orders.valid_order_date) as most_recent_non_returned_order_date,
+
+        coalesce(max(orders.user_order_seq),0) as order_count,
+
+        coalesce(count(case 
+            when orders.valid_order_date is not null
+            then 1 
+        end),0) as non_returned_order_count,
 
         sum(case 
-            when a.status not in ('returned','return_pending') 
-            then round(c.amount/100.0,2) 
-            else 0 
+            when orders.valid_order_date is not null
+            then orders.order_value_dollars else 0 
         end) as total_lifetime_value,
 
         sum(case 
-            when a.status not in ('returned','return_pending') 
-            then round(c.amount/100.0,2) else 0 
-        end)/
-        nullif(count(case 
-           when a.status not in ('returned','return_pending') 
-           then 1 end),
-           0) 
-        as avg_non_returned_order_value,
+            when orders.valid_order_date is not null
+            then orders.order_value_dollars else 0 
+            else 0 
+        end)
+        / nullif(count(case 
+            when orders.valid_order_date is not null
+            then 1 
+        end),0) as avg_non_returned_order_value,
 
-        array_agg(distinct a.id) as order_ids
+        array_agg(distinct orders.order_id) as order_ids
 
-    from (
-      select 
-        row_number() over (partition by user_id order by order_date, id) as user_order_seq,
-        *
-      from orders
-    ) a
+    from orders
 
-    join ( 
-      select 
-        first_name || ' ' || last_name as name, 
-        * 
-      from customers
-    ) b
-    on a.user_id = b.id
+    join customers
+    on orders.customer_id = customers.customer_id
 
-    left outer join payments c
-    on a.id = c.orderid
+    group by customers.customer_id, customers.full_name, customers.surname, customers.givenname,
+),
 
-    where a.status not in ('pending') and c.status != 'fail'
+-- Final CTEs 
+final as (
 
-    group by b.id, b.name, b.last_name, b.first_name
+    select 
 
-) customer_order_history
-on orders.user_id = customer_order_history.customer_id
+        orders.order_id,
+        orders.customer_id,
+        customers.surname,
+        customers.givenname,
+        first_order_date,
+        order_count,
+        total_lifetime_value,
+        orders.order_value_dollars,
+        orders.order_status,
+        orders.payment_status
 
-left outer join payments
-on orders.id = payments.orderid
+    from orders
 
-where payments.status != 'fail'
+    join customers
+    on orders.customer_id = customers.customer_id
+
+    join customer_order_history
+    on orders.customer_id = customer_order_history.customer_id
+
+)
+
+-- Simple Select Statement
+select * from final
